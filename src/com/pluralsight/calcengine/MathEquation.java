66708fd712dfe9aabb6b3056f464dec5fbc57560
emekdahl <emily@otus.com>
2019-05-28 05:51:06 -0500

    move constructors to the top

--- src/com/pluralsight/calcengine/MathEquation.java
+++ src/com/pluralsight/calcengine/MathEquation.java
@@ -2,6 +2,17 @@ package com.pluralsight.calcengine;
 
 public class MathEquation {
 
+    public MathEquation () {}
+
+    public MathEquation (char opCode) {
+        this.opCode = opCode;
+    }
+
+    public MathEquation (char opCode, double leftVal, double rightVal) {
+        this (opCode);
+        this.leftVal = leftVal;
+        this.rightVal = rightVal;
+    }
 
     private double leftVal;
     private double rightVal;
@@ -34,18 +45,6 @@ public class MathEquation {
 
     public double getResult() {
         return result;
-    }
-
-    public MathEquation (char opCode) {
-        this.opCode = opCode;
-    }
-
-    public MathEquation() {}
-
-    public MathEquation (char opCode, double leftVal, double rightVal) {
-        this (opCode);
-        this.leftVal = leftVal;
-        this.rightVal = rightVal;
     }
 
     public void execute() {
